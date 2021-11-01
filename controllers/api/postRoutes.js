dbe54a004262ab89395076503b745e1d877fbb1e
Juan Jose Ramirez <jjramirez@miuandes.cl>
2021-10-31 20:42:52 -0700

    post model and seed

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+router.post('/', (req,res) => {
+    // add logic for new posts
+});
+
+router.delete('/', (req, res) => {
+    // add logic to delete an existing post
+})
